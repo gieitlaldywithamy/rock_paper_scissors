2c8307893c31f5b87e45713a3ac1f40dd8d6508d
Amy <amy.jeanette.morrson@gmail.com>
2017-12-12 16:23:50 +0000

    added test suite

--- models/game.rb
+++ models/game.rb
@@ -6,13 +6,15 @@ class Game
   end
 
   def who_wins()
+
+
   end
 end
 
-Create a simple sinatra app to allow the user to play rock, paper, scissors in their browser.
-
-You should be able to go to /rock/scissors and return the string "rock wins" to the page, for example.
-
-You will need to write a bit of logic that compares the two hands and decides which one has won, and then use that in the route definition.
-
-Next, create a models directory for your game logic class e.g. 'Game' and put all of the logic in there - there shouldn't be any logic left in the controller!
+# Create a simple sinatra app to allow the user to play rock, paper, scissors in their browser.
+#
+# You should be able to go to /rock/scissors and return the string "rock wins" to the page, for example.
+#
+# You will need to write a bit of logic that compares the two hands and decides which one has won, and then use that in the route definition.
+#
+# Next, create a models directory for your game logic class e.g. 'Game' and put all of the logic in there - there shouldn't be any logic left in the controller!
